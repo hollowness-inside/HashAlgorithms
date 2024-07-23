13ab246395f11f9242918982fcea1873e363e682
hollowness-inside <malikov.vp@students.dvfu.ru>
2024-07-23 15:35:55 +1000

    Rename package

--- examples/hash512.rs
+++ examples/hash512.rs
@@ -1,7 +1,8 @@
-use HashAlgorithms::HashBytes;
+use hash_algorithms::sha::Sha512;
+use hash_algorithms::HashBytes;
 
 fn main() {
-    let digest = HashAlgorithms::sha::Sha512::hash_bytes(b"Hello World");
+    let digest = Sha512::hash_bytes(b"Hello World");
     let hex: String = digest.into_iter().map(|byte| format!("{byte:X}")).collect();
 
     println!("{hex}");
